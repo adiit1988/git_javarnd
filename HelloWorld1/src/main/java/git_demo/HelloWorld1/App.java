939df9d37f367308d70b2a2abcf509c1164dd069
Aditya Agrawal <adi_it1988@yahoo.co.in>
2021-12-14 22:16:18 +0530

    first Java project

---
+++ HelloWorld1/src/main/java/git_demo/HelloWorld1/App.java
@@ -0,0 +1,13 @@
+package git_demo.HelloWorld1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
